c227ddf3978c77b4e2e4113a520d62b867d06fe0
Soyn <w604408482@163.com>
2015-05-22 11:54:46 +0800

    test handwriting

--- Support Vector/svmMLiA.py
+++ Support Vector/svmMLiA.py
@@ -330,6 +330,66 @@ def testRbf(k1 = 1.3):
                 errorCount += 1
         print "the test error rate is: %f" % (float (errorCount) / m)
 
+def img2Vector(filename):
+    '''
+    将图片转换为1X1024的vector，用Numpy的array存储
+    :param filename:
+    :return:
+    '''
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+def loadImages(dirName):
+    from os import  listdir
+    hwLabels = []
+    trainingFileList = listdir(dirName)
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int (fileStr.split('_')[0])
+        if classNumStr == 9:
+            hwLabels.append(-1)
+        else:
+            hwLabels.append(1)
+        trainingMat[i, :] = img2Vector('%s/%s ' % (dirName, fileNameStr))
+    return trainingMat,hwLabels
+
+def testDigits(kTup = ('rbf', 10)):
+    dataArr, labelArr = loadImages('trainingDigits')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, kTup)
+    dataMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    svInd = np.nonzero(alphas.A > 0)[0]
+    sVs = dataMat[svInd]
+    labelSV = labelMat[svInd]
+    print "there are %d Support Vectors." % np.shape(sVs)[0]
+    m,n = np.shape(dataMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, dataMat[i, :], kTup)
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]):
+            errorCount += 1
+        print "the training error rate is: %f " % (float(errorCount) / m)
+        dataArr,  labelArr = loadImages('testDigits')
+        errorCount = 0
+        dataMat = np.mat(dataArr)
+        labelMat = np.mat(labelArr).transpose()
+        m, n = np.shape(dataMat)
+        for i in range(m):
+            kernelEval = kernelTrans(sVs, dataMat[i, :], kTup)
+            predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+            if np.sign(predict) != np.sign(labelArr[i]):
+                errorCount += 1
+        print "the test error rate is: %f" % (float(errorCount) / m)
+
 
 if __name__ == "__main__":
-    testRbf()
+    testDigits(('rbf', 20))
